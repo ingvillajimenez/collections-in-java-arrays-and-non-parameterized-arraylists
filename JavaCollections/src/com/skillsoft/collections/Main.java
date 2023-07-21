c8e3f751519942b7e3ffb571ddd1553fe33c2514
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-21 17:40:46 -0600

    Working with Arrays as Collection Containers

--- JavaCollections/src/com/skillsoft/collections/Main.java
+++ JavaCollections/src/com/skillsoft/collections/Main.java
@@ -4,65 +4,68 @@ import java.util.Arrays;
 
 public class Main {
 
+    private static String[] stringArray = new String[5];
+    private static int lastIndex = 0;
+
+    private static void addString(String element) {
+        if (lastIndex >= stringArray.length) {
+            System.out.println("Cannot add element, no room in array!");
+
+            return;
+        }
+
+        stringArray[lastIndex++] = element;
+    }
+
+    private static void deleteString(int index) {
+        if (index >= stringArray.length) {
+            System.out.println("Index out of bounds, cannot delete element");
+
+            return;
+        }
+
+        stringArray[index] = null;
+
+        int currentIndex = index;
+
+        for (int i = currentIndex; i < stringArray.length - 1; i++) {
+            stringArray[currentIndex] = stringArray[currentIndex + 1];
+            currentIndex++;
+        }
+
+        lastIndex--;
+    }
+
     public static void main(String[] args) {
 
-        Object[] mixedArray = {"Amazon", 100.0f, 23, true};
-
-        System.out.println("Array contents: " + Arrays.toString(mixedArray));
-
-        System.out.println();
-
-        for (Object element: mixedArray) {
-            System.out.format("element class: %s", element.getClass());
-            System.out.println();
-
-            System.out.format("element: %s", element);
-            System.out.println();
-        }
+        addString("Amazon");
+        System.out.println("Array contents: " + Arrays.toString(stringArray));
 
         System.out.println();
 
-        String[] companies = {"Amazon", "Google", "Microsoft", "Facebook"};
-
-        System.out.println("Array contents: " + Arrays.toString(companies));
-
-        System.out.println();
-        System.out.println("**** Specifying indexes manually");
-
-        for (int i = 0; i < 4; i++) {
-            System.out.format("companies[%d]: %s", i, companies[i]);
-            System.out.println();
-        }
+        addString("Skillsoft");
+        System.out.println("Array contents: " + Arrays.toString(stringArray));
 
         System.out.println();
 
-//        System.out.println("**** Off-by-one error while specifying indexes manually");
-//
-//        for (int i = 0; i < 5; i++) {
-//            System.out.format("companies[%d]: %s", i, companies[i]); // java.lang.ArrayIndexOutOfBoundsException
-//            System.out.println();
-//        }
-//
-//        System.out.println();
-
-        System.out.println("**** Use array length instead of specifying indexes manually");
-
-        for (int i = 0; i < companies.length; i++) {
-            System.out.format("companies[%d]: %s", i, companies[i]);
-            System.out.println();
-        }
+        addString("SumTotal");
+        System.out.println("Array contents: " + Arrays.toString(stringArray));
 
         System.out.println();
 
-        System.out.println("**** Even better, use a for-each loop to iterate over arrays");
-
-        for (String company: companies) {
-            System.out.format("company %s", company);
-            System.out.println();
-        }
+        addString("Suzuki");
+        System.out.println("Array contents: " + Arrays.toString(stringArray));
 
         System.out.println();
 
+        deleteString(0);
+        System.out.println("Array contents: " + Arrays.toString(stringArray));
+
+        deleteString(1);
+        System.out.println("Array contents: " + Arrays.toString(stringArray));
+
+        addString("Optum");
+        System.out.println("Array contents: " + Arrays.toString(stringArray));
     }
 }
 
