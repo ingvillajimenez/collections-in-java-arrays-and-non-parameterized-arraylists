961d5d3111bda91636ceb134ee58438abbd6f178
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-21 16:43:17 -0600

    Iterating over Arrays Using For Loops

--- JavaCollections/src/com/skillsoft/collections/Main.java
+++ JavaCollections/src/com/skillsoft/collections/Main.java
@@ -1,82 +1,67 @@
 package com.skillsoft.collections;
 
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Object[] mixedArray = {"Amazon", 100.0f, 23, true};
+
+        System.out.println("Array contents: " + Arrays.toString(mixedArray));
+
+        System.out.println();
+
+        for (Object element: mixedArray) {
+            System.out.format("element class: %s", element.getClass());
+            System.out.println();
+
+            System.out.format("element: %s", element);
+            System.out.println();
+        }
+
+        System.out.println();
 
         String[] companies = {"Amazon", "Google", "Microsoft", "Facebook"};
 
         System.out.println("Array contents: " + Arrays.toString(companies));
 
         System.out.println();
+        System.out.println("**** Specifying indexes manually");
 
-        System.out.println("companies[0]: " + companies[0]);
-        System.out.println("companies[1]: " + companies[1]);
-        System.out.println("companies[2]: " + companies[2]);
-        System.out.println("companies[3]: " + companies[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.format("companies[%d]: %s", i, companies[i]);
+            System.out.println();
+        }
 
         System.out.println();
 
-//        System.out.println("companies[4]: " + companies[4]); // java.lang.ArrayIndexOutOfBoundsException
-
-        System.out.println("companies[-1]: " + companies[-1]); // java.lang.ArrayIndexOutOfBoundsException
-
-//        String[] companies = {};
+//        System.out.println("**** Off-by-one error while specifying indexes manually");
 //
-//        String[] otherCompanies = new String[0];
-//
-//        System.out.println("companies.length: " + companies.length);
-//        System.out.println("otherCompanies.length: " + otherCompanies.length);
-//
-//        companies[0] = "Skillsoft"; // java.lang.ArrayIndexOutOfBoundsException
-
-//        otherCompanies[0] = "Skillsoft"; // java.lang.ArrayIndexOutOfBoundsException
-
-//        String[] companies = new String[]; // java: array dimension missing
-//
-//        companies[0] = "Amazon";
-
-//        int[] intArray = new int[4];
-//
-//        intArray[0] = 10;
-//        intArray[1] = 20;
-//
-//        float[] floatArray = new float[6];
-//        floatArray[0] = 10.5f;
-//        floatArray[3] = 20.5f;
-//
-//        String[] stringArray = new String[7];
-//        stringArray[1] = "Good";
-//        stringArray[2] = "Morning";
-//
-//        Date[] dateArray = new Date[10];
-//        dateArray[1] = Calendar.getInstance().getTime();
-//        dateArray[2] = Calendar.getInstance().getTime();
-//
-//        System.out.println("intArray instanceof Object: " + (intArray instanceof Object));
-//        System.out.println("floatArray instanceof Object: " + (floatArray instanceof Object));
-//        System.out.println("stringArray instanceof Object: " + (stringArray instanceof Object));
-//        System.out.println("dateArray instanceof Object: " + (dateArray instanceof Object));
+//        for (int i = 0; i < 5; i++) {
+//            System.out.format("companies[%d]: %s", i, companies[i]); // java.lang.ArrayIndexOutOfBoundsException
+//            System.out.println();
+//        }
 //
 //        System.out.println();
-//
-//        System.out.println("\nintArray.toString(): " + Arrays.toString(intArray));
-//        System.out.println("intArray.length: " + intArray.length);
-//
-//        System.out.println("\nfloatArray.toString(): " + Arrays.toString(floatArray));
-//        System.out.println("floatArray.length: " + floatArray.length);
-//
-//        System.out.println("\nstringArray.toString(): " + Arrays.toString(stringArray));
-//        System.out.println("stringArray.length: " + stringArray.length);
-//
-//        System.out.println("\ndateArray.toString(): " + Arrays.toString(dateArray));
-//        System.out.println("dateArray.length: " + dateArray.length);
-//
-//        System.out.println();
+
+        System.out.println("**** Use array length instead of specifying indexes manually");
+
+        for (int i = 0; i < companies.length; i++) {
+            System.out.format("companies[%d]: %s", i, companies[i]);
+            System.out.println();
+        }
+
+        System.out.println();
+
+        System.out.println("**** Even better, use a for-each loop to iterate over arrays");
+
+        for (String company: companies) {
+            System.out.format("company %s", company);
+            System.out.println();
+        }
+
+        System.out.println();
 
     }
 }
